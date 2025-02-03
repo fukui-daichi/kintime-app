9f92ec2d8bee320ebfeb9f1375c2dad2387c775c
fukui-daichi <k.fukui0913@gmail.com>
2025-02-03 20:04:21 +0900

    [Fix] ルーティングを整理

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
 // トップページ
 Route::get('/', [HomeController::class, 'index'])
     ->name('home');
@@ -14,41 +13,33 @@ Route::get('/', [HomeController::class, 'index'])
 // 認証が必要なルート
 Route::middleware('auth')->group(function () {
     // プロフィール関連
-    Route::get('/profile', [ProfileController::class, 'edit'])
-        ->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])
-        ->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])
-        ->name('profile.destroy');
+    Route::prefix('profile')->name('profile.')->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
+    });
 
     // 勤怠関連
-    Route::get('/attendance', [AttendanceController::class, 'monthly'])
-        ->name('attendance.monthly');  // 月別一覧画面
-    Route::post('/attendance/clock-in', [AttendanceController::class, 'clockIn'])
-        ->name('attendance.clockIn');
-    Route::post('/attendance/clock-out', [AttendanceController::class, 'clockOut'])
-        ->name('attendance.clockOut');
+    Route::prefix('attendance')->name('attendance.')->group(function () {
+        Route::get('/', [AttendanceController::class, 'monthly'])->name('monthly');
+        Route::post('/clock-in', [AttendanceController::class, 'clockIn'])->name('clockIn');
+        Route::post('/clock-out', [AttendanceController::class, 'clockOut'])->name('clockOut');
+    });
 
     // 申請関連のルート
     Route::prefix('requests')->name('requests.')->group(function () {
         // 一般ユーザー用のルート
         Route::middleware(['auth'])->group(function () {
-            Route::get('/', [ApprovalRequestController::class, 'userIndex'])
-                ->name('index');  // /requests
-            Route::get('/create/{attendance}', [ApprovalRequestController::class, 'create'])
-                ->name('create'); // /requests/create/{attendance}
-            Route::post('/', [ApprovalRequestController::class, 'store'])
-                ->name('store');  // /requests (POST)
+            Route::get('/', [ApprovalRequestController::class, 'userIndex'])->name('index');
+            Route::get('/create/{attendance}', [ApprovalRequestController::class, 'create'])->name('create');
+            Route::post('/', [ApprovalRequestController::class, 'store'])->name('store');
         });
 
         // 管理者用のルート
         Route::middleware(['admin'])->group(function () {
-            Route::get('/admin', [ApprovalRequestController::class, 'adminIndex'])
-                ->name('admin.index'); // /requests/admin
-            Route::patch('/{request}/approve', [ApprovalRequestController::class, 'approve'])
-                ->name('approve');      // /requests/{request}/approve
-            Route::patch('/{request}/reject', [ApprovalRequestController::class, 'reject'])
-                ->name('reject');       // /requests/{request}/reject
+            Route::get('/admin', [ApprovalRequestController::class, 'adminIndex'])->name('admin.index');
+            Route::patch('/{request}/approve', [ApprovalRequestController::class, 'approve'])->name('approve');
+            Route::patch('/{request}/reject', [ApprovalRequestController::class, 'reject'])->name('reject');
         });
     });
 });
