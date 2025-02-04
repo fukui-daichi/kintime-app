f7890fbb9555b222da930f3bd3c9d1b2c1b15003
fukui-daichi <k.fukui0913@gmail.com>
2025-02-04 10:14:06 +0900

    ビューを整理

--- resources/views/components/admin/navigation.blade.php
+++ resources/views/components/admin/navigation.blade.php
@@ -12,9 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="'/'" :active="request()->is('/')">
-                        {{ Auth::user()->user_type === 'admin' ? '管理者ダッシュボード' : 'マイページ' }}
-                    </x-nav-link>
+                    <x-nav-link :href="'/'" :active="request()->is('/')">ダッシュボード</x-nav-link>
                 </div>
             </div>
 
@@ -23,7 +21,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->last_name }} {{ Auth::user()->first_name }}</div>
+                            <div>管理者</div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -67,15 +65,13 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="'/'" :active="request()->is('/')">
-                {{ Auth::user()->user_type === 'admin' ? '管理者ダッシュボード' : 'マイページ' }}
-            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="'/'" :active="request()->is('/')">ダッシュボード</x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->last_name }} {{ Auth::user()->first_name }}</div>
+                <div class="font-medium text-base text-gray-800">管理者</div>
                 <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+            <x-admin.navigation />
 
             <!-- Page Heading -->
             @isset($header)
@@ -34,3 +34,4 @@
         </div>
     </body>
 </html>
+
