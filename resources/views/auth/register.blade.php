3530e09df7d760778ab4c02e9d23f595b61ad01c
fukui-daichi <k.fukui0913@gmail.com>
2025-04-24 09:40:44 +0900

    ユーザー登録・編集画面の拡張

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,11 +2,67 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        <!-- 姓・名 -->
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+                <x-input-label for="last_name" :value="__('姓')" />
+                <x-text-input id="last_name" name="last_name" type="text" class="mt-1 block w-full" :value="old('last_name')" required autocomplete="family-name" autofocus />
+                <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+            </div>
+            <div>
+                <x-input-label for="first_name" :value="__('名')" />
+                <x-text-input id="first_name" name="first_name" type="text" class="mt-1 block w-full" :value="old('first_name')" required autocomplete="given-name" />
+                <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
+            </div>
+        </div>
+
+        <!-- 社員番号 -->
+        <div class="mt-4">
+            <x-input-label for="employee_number" :value="__('社員番号')" />
+            <x-text-input id="employee_number" name="employee_number" type="text" class="mt-1 block w-full" :value="old('employee_number')" required />
+            <x-input-error :messages="$errors->get('employee_number')" class="mt-2" />
+        </div>
+
+        <!-- 部署 -->
+        <div class="mt-4">
+            <x-input-label for="department_id" :value="__('部署')" />
+            <select id="department_id" name="department_id" class="mt-1 block w-full" required>
+                <option value="">選択してください</option>
+                @foreach($departments as $department)
+                    <option value="{{ $department->id }}" @selected(old('department_id') == $department->id)>
+                        {{ $department->name }}
+                    </option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('department_id')" class="mt-2" />
+        </div>
+
+        <!-- 雇用形態 -->
+        <div class="mt-4">
+            <x-input-label for="employment_type" :value="__('雇用形態')" />
+            <select id="employment_type" name="employment_type" class="mt-1 block w-full" required>
+                <option value="">選択してください</option>
+                @foreach($employmentTypes as $type)
+                    <option value="{{ $type }}" @selected(old('employment_type') == $type)>
+                        {{ $type }}
+                    </option>
+                @endforeach
+            </select>
+            <x-input-error :messages="$errors->get('employment_type')" class="mt-2" />
+        </div>
+
+        <!-- 入社日・退社日 -->
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
+            <div>
+                <x-input-label for="joined_at" :value="__('入社日')" />
+                <x-text-input id="joined_at" name="joined_at" type="date" class="mt-1 block w-full" :value="old('joined_at')" required />
+                <x-input-error :messages="$errors->get('joined_at')" class="mt-2" />
+            </div>
+            <div>
+                <x-input-label for="leaved_at" :value="__('退社日（任意）')" />
+                <x-text-input id="leaved_at" name="leaved_at" type="date" class="mt-1 block w-full" :value="old('leaved_at')" />
+                <x-input-error :messages="$errors->get('leaved_at')" class="mt-2" />
+            </div>
         </div>
 
         <!-- Email Address -->
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -17,10 +17,62 @@
         @csrf
         @method('patch')
 
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+                <x-input-label for="last_name" :value="__('姓')" />
+                <x-text-input id="last_name" name="last_name" type="text" class="mt-1 block w-full" :value="old('last_name', $user->last_name)" required autocomplete="family-name" />
+                <x-input-error class="mt-2" :messages="$errors->get('last_name')" />
+            </div>
+            <div>
+                <x-input-label for="first_name" :value="__('名')" />
+                <x-text-input id="first_name" name="first_name" type="text" class="mt-1 block w-full" :value="old('first_name', $user->first_name)" required autocomplete="given-name" />
+                <x-input-error class="mt-2" :messages="$errors->get('first_name')" />
+            </div>
+        </div>
+
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            <x-input-label for="employee_number" :value="__('社員番号')" />
+            <x-text-input id="employee_number" name="employee_number" type="text" class="mt-1 block w-full" :value="old('employee_number', $user->employee_number)" required />
+            <x-input-error class="mt-2" :messages="$errors->get('employee_number')" />
+        </div>
+
+        <div>
+            <x-input-label for="department_id" :value="__('部署')" />
+            <select id="department_id" name="department_id" class="mt-1 block w-full">
+                <option value="">選択してください</option>
+                @foreach($departments as $department)
+                    <option value="{{ $department->id }}" @selected(old('department_id', $user->department_id) == $department->id)>
+                        {{ $department->name }}
+                    </option>
+                @endforeach
+            </select>
+            <x-input-error class="mt-2" :messages="$errors->get('department_id')" />
+        </div>
+
+        <div>
+            <x-input-label for="employment_type" :value="__('雇用形態')" />
+            <select id="employment_type" name="employment_type" class="mt-1 block w-full">
+                <option value="">選択してください</option>
+                @foreach($employmentTypes as $type)
+                    <option value="{{ $type }}" @selected(old('employment_type', $user->employment_type) == $type)>
+                        {{ $type }}
+                    </option>
+                @endforeach
+            </select>
+            <x-input-error class="mt-2" :messages="$errors->get('employment_type')" />
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+                <x-input-label for="joined_at" :value="__('入社日')" />
+                <x-text-input id="joined_at" name="joined_at" type="date" class="mt-1 block w-full" :value="old('joined_at', $user->joined_at)" required />
+                <x-input-error class="mt-2" :messages="$errors->get('joined_at')" />
+            </div>
+            <div>
+                <x-input-label for="leaved_at" :value="__('退社日（任意）')" />
+                <x-text-input id="leaved_at" name="leaved_at" type="date" class="mt-1 block w-full" :value="old('leaved_at', $user->leaved_at)" />
+                <x-input-error class="mt-2" :messages="$errors->get('leaved_at')" />
+            </div>
         </div>
 
         <div>
