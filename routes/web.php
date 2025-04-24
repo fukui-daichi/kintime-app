bdadb2b95bde96a48fb1288a15e7b068d34ad828
fukui-daichi <k.fukui0913@gmail.com>
2025-04-24 13:03:17 +0900

    トップページへの遷移

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,6 @@ Route::get('/', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
