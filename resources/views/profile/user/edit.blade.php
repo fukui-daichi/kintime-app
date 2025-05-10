54d333d039684194df404c958064828de14bd8ee
fukui-daichi <k.fukui0913@gmail.com>
2025-05-10 15:58:37 +0900

    プロフィール更新機能の修正

--- resources/views/profile/user/edit.blade.php
+++ resources/views/profile/user/edit.blade.php
@@ -107,7 +107,7 @@
                             </div>
 
                             <div class="flex items-center gap-4">
-                                <x-primary-button>{{ __('Save') }}</x-primary-button>
+                                <x-primary-button>保存</x-primary-button>
 
                                 @if (session('status') === 'profile-updated')
                                     <p
@@ -116,7 +116,7 @@
                                         x-transition
                                         x-init="setTimeout(() => show = false, 2000)"
                                         class="text-sm text-gray-600 dark:text-gray-400"
-                                    >{{ __('Saved.') }}</p>
+                                    >保存しました</p>
                                 @endif
                             </div>
                         </form>
@@ -156,7 +156,7 @@
                             </div>
 
                             <div class="flex items-center gap-4">
-                                <x-primary-button>{{ __('Save') }}</x-primary-button>
+                                <x-primary-button>保存</x-primary-button>
 
                                 @if (session('status') === 'password-updated')
                                     <p
@@ -165,7 +165,7 @@
                                         x-transition
                                         x-init="setTimeout(() => show = false, 2000)"
                                         class="text-sm text-gray-600 dark:text-gray-400"
-                                    >{{ __('Saved.') }}</p>
+                                    >保存しました</p>
                                 @endif
                             </div>
                         </form>
